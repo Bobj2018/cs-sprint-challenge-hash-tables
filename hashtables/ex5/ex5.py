8f1bd5ef518c388b6c705a6c2123ecb405a41b37
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-11-06 23:33:46 -0500

    complete ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -3,23 +3,46 @@
 
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hash_table = {}
+
+
+    #hash files by storing the characters after the hash
+    for i in files:
+        curr_char = len(i) - 1
+
+        while i[curr_char - 1] != "/":
+            curr_char -= 1
+
+        if i[curr_char:len(i)] in hash_table:
+            hash_table[i[curr_char:len(i)]].append(i)
+        else:
+            hash_table[i[curr_char:len(i)]] = [i]
+
+
+
+    #loops through queries and append to result if exists
+
+    result = []
+    for i in queries:
+        if i in hash_table:
+            for j in hash_table[i]:
+                    result.append(j)
+
 
     return result
 
 
 if __name__ == "__main__":
     files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
+    "/usr/local/share/foo.txt",
+    "/usr/bin/ls",
+    "/home/davidlightman/foo.txt",
+    "/bin/su"
+]
+
     queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+    "ls",
+    "foo.txt",
+    "nosuchfile.txt"
+]
     print(finder(files, queries))
