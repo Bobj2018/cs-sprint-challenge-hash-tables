fdfe068f0199103f0a7edc9b02e537e8a7a8edab
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-11-07 00:17:10 -0500

    work on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hash_table = {}
+    result = []
 
-    return None
+    #store the weight as keys and the index as values
+    for i in range(length):
+        hash_table[weights[i]] = i
+
+    for weight in weights:
+        if (limit - weight) in hash_table:
+            result.append(hash_table[limit - weight])
+
+    if len(result) == 0:
+        return None
+    else:
+        result.sort(reverse=True)
+        return result
+
+
+x = get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
+print(x)
